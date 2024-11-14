d52709c9ac13a4655121c01968885c7489685b87
Franoll Fantu <fantu.franoll1@gmail.com>
2024-11-13 16:14:36 -0800

    completed text-to-speech feature

---
+++ src/app/mainmenu/text-to-speech/page.js
@@ -0,0 +1,9 @@
+
+import TextToSpeechButton from '../../../../components/TextToSpeechButton'
+export default function Home() {
+  return (
+    <div>
+      <TextToSpeechButton />
+    </div>
+  );
+}
