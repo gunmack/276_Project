0cf76bdc3644439a04d9be0177e2c14b609801bc
gunmack <jreedoy2002@yahoo.com>
2024-11-16 16:34:15 -0800

    removed next.config.js ( in conflict with next.config.mjs )

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizeFonts: true // Enables SWC optimization for fonts (needed for next/font)
-  }
-  // You can add more Next.js configuration here if needed
-};
-
-module.exports = nextConfig;
