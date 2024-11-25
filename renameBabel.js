7b8a219ffb3ac5a0962f172e355fa87e8da8e675
Julkar Reedoy <91394056+gunmack@users.noreply.github.com>
2024-11-24 17:19:35 -0800

    little tweak to running JEST tests (#41)
    * added a script to rename babel file before and
    after the test run, and removed the command from
    the pipeline.yml file
    
    * forgot to remove revert statement from
    pipeline.yml
    
    * modified:   .husky/pre-commit
    
    * Added test-reports/ to .gitignore as it has
    been a constant source of merge conflict.
    It regenerates every time the tests are run.

---
+++ renameBabel.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+// Specify the old and new file paths
+const oldFilePath = path.join(__dirname, 'babel.config.js.backup'); // Replace with your old file path
+const newFilePath = path.join(__dirname, 'babel.config.js'); // Replace with your desired new file path
+
+// Rename the file
+fs.rename(oldFilePath, newFilePath, (err) => {
+  if (err) {
+    console.error('Error renaming file:', err);
+    process.exit(1); // Exit with error code
+  } else {
+    console.log(`Renamed ${oldFilePath} to ${newFilePath}`);
+  }
+});
