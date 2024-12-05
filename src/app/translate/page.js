a6270758b61e0a12df58820352a43639f58cc5f9
gunmack <jreedoy2002@yahoo.com>
2024-12-04 17:28:25 -0800

    removed light mode theme from css file

--- src/app/translate/page.js
+++ src/app/translate/page.js
@@ -11,7 +11,7 @@ export default function LearnVocab() {
     <>
       <div
         data-testid="Learn Vocab"
-        className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-ibmPlexMono)] bg-gradient-to-r"
+        className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-ibmPlexMono)] bg-gradient-to-r"
       >
         <Toolbar />
 
@@ -41,7 +41,7 @@ export default function LearnVocab() {
         </button>
 
         <main className="flex flex-col gap-8 row-start-2 items-center justify-center sm:items-start">
-          <div className="flex flex-col justify-center items-center p-8 gap-4 font-[family-name:var(--font-geist-mono)]">
+          <div className=" font-[family-name:var(--font-geist-mono)]">
             <VocabBox />
           </div>
         </main>
